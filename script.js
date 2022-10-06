360a1df6c0e2ebfe83c57a54c3e8dd3049d27195
Ali-Mugamai <Ali-Mugamai@users.noreply.github.com>
2022-10-06 18:45:20 +0000

    Prettified Code!

--- script.js
+++ script.js
@@ -7,10 +7,15 @@
 /**
  * Do basic math.
  */
- function doMathClicked() {
-    document.getElementById("add-math").innerHTML = "<p>6 + 5 = " + (6 + 5) + "</p>"
-    document.getElementById("subtract-math").innerHTML = "<p>7 - 3 =  " + (7 - 3) + "</p>"  
-    document.getElementById("multiply-math").innerHTML = "<p>3 + 4 × 2 = " + (3 + 4 * 2) + "</p>" 
-    document.getElementById("divide-math").innerHTML = "<p>(4 ÷ 2) + 3 = " + (4 / 2 + 3) + "</p>"
-    document.getElementById("exponent-math").innerHTML = "<p>5 + 2³ = " + (5 + 2 ** 3) + "</p>"
-  }  
+function doMathClicked() {
+  document.getElementById("add-math").innerHTML =
+    "<p>6 + 5 = " + (6 + 5) + "</p>"
+  document.getElementById("subtract-math").innerHTML =
+    "<p>7 - 3 =  " + (7 - 3) + "</p>"
+  document.getElementById("multiply-math").innerHTML =
+    "<p>3 + 4 × 2 = " + (3 + 4 * 2) + "</p>"
+  document.getElementById("divide-math").innerHTML =
+    "<p>(4 ÷ 2) + 3 = " + (4 / 2 + 3) + "</p>"
+  document.getElementById("exponent-math").innerHTML =
+    "<p>5 + 2³ = " + (5 + 2 ** 3) + "</p>"
+}
