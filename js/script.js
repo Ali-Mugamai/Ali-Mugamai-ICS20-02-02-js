23f29f97a7c3b27bd30f5623ad45ba30a2863cdf
Ali-Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-10-06 14:34:39 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Ali Mugamai
+// Created on: Sep 2022
+// This file contains the JS functions for index.html
+
+/**
+ * This function displays "Hello, World!".
+ * @constructor
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
